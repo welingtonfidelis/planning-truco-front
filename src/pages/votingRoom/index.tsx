536cb6f0a96493bf6b52796a0c0a5d07bb27ebd1
Welington Fidelis <welingtonfidelis@gmail.com>
2024-02-26 09:02:30 -0300

    Init socket connect: connection, roomdata, new user room and user room logout done;

--- src/pages/votingRoom/index.tsx
+++ src/pages/votingRoom/index.tsx
@@ -2,18 +2,31 @@ import { ApplicationRoutes } from "../../shared/enum/applicationRoutes";
 import { Container, Content, TableContent, TableIcon } from "./styles";
 import { userStore } from "../../store/user";
 import { roomStore } from "../../store/room";
-import { useNavigate } from "react-router-dom";
-import { useEffect, useMemo } from "react";
+import { createSearchParams, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { PageHeader } from "../../components/pageHeader";
 import { FloatActionButtons } from "./components/floatActionButtons";
 import { User } from "../../domains/user";
-import { UserRoom } from "../../domains/room";
+import { Room, UserRoom } from "../../domains/room";
 import { UserCard } from "./components/userCard";
-import { Button, background } from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import { Deck } from "./components/deck";
+import { Socket, io } from "socket.io-client";
+import { config } from "../../config";
+import { KnownError } from "../../domains/knownError";
+import { ServerError } from "../../shared/enum/serverError";
+import { urlParams } from "../../services/util/urlParams";
+import { SocketEvents } from "../../shared/enum/socketEvents";
 
 const { ROOT } = ApplicationRoutes;
+const { INVALID_ROOM, MISSING_ROOM } = ServerError;
+const {
+  EXCEPTION,
+  ROOM_DATA,
+  ROOM_NEW_USER,
+  ROOM_USER_LOGOUT,
+} = SocketEvents;
 
 enum ChairPositionEnum {
   TOP = "top",
@@ -22,58 +35,97 @@ enum ChairPositionEnum {
   LEFT = "left",
 }
 
-
 export const VotingRoom = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const { getParams } = urlParams();
+  const toast = useToast();
 
-  const { id: userId, name } = userStore();
-  const { id: roomId, showVotes, ownerUserId, updateRoom } = roomStore();
+  const { id: userId, name: userName } = userStore();
+  const {
+    id: roomId,
+    showVotes,
+    ownerUserId,
+    updateRoom,
+    users: usersOnRoom,
+    addUser,
+    removeUser,
+  } = roomStore();
 
-  useEffect(() => {
-    if (!roomId) {
-      navigate(ROOT);
+  let socketIo: Socket;
+
+  const chairOrganize = usersOnRoom.reduce(
+    (acc, user) => {
+      if (user.id === ownerUserId) acc[ChairPositionEnum.TOP].push(user);
+      else acc[ChairPositionEnum.BOTTOM].push(user);
+
+      return acc;
+    },
+    {
+      [ChairPositionEnum.TOP]: [] as UserRoom[],
+      [ChairPositionEnum.BOTTOM]: [] as UserRoom[],
     }
-  }, []);
-
-  const usersMock = [
-    { name: "Fulano A", id: "1", vote: 0 },
-    { name: "Fulano nome longo longo longo B", id: "2", vote: 0 },
-    { name: "Fulano C", id: "3", vote: 0 },
-    { name: "Fulano D", id: "4", vote: 0 },
-    { name: "Fulano E", id: "5", vote: 0 },
-    { name: "Fulano F", id: "6", vote: 0 },
-    // { name: "Fulano G", id: "7", vote: 0 },
-    // { name: "Fulano H", id: "8", vote: 0 },
-    // { name: "Fulano nome maior I", id: "9", vote: 0 },
-    // { name: "Fulano J", id: "10", vote: 0 },
-    // { name: "Fulano K", id: "11", vote: 0 },
-    // { name: "Fulano G", id: "12", vote: 0 },
-    // { name: "Fulano H", id: "13", vote: 0 },
-    // { name: "Fulano nome maior I", id: "14", vote: 0 },
-    // { name: "Fulano J", id: "15", vote: 0 },
-    // { name: "Fulano K", id: "16", vote: 0 },
-    { name, id: userId, vote: 0 },
-  ];
-
-  const chairOrganize = useMemo(() => {
-    return usersMock.reduce(
-      (acc, user) => {
-        if (user.id === ownerUserId) acc[ChairPositionEnum.TOP].push(user);
-        else acc[ChairPositionEnum.BOTTOM].push(user);
-
-        return acc;
-      },
-      {
-        [ChairPositionEnum.TOP]: [] as UserRoom[],
-        [ChairPositionEnum.BOTTOM]: [] as UserRoom[],
-      }
-    );
-  }, [usersMock]);
+  );
 
   const handleShowVotes = (showVotes: boolean) => {
     updateRoom({ showVotes });
   };
+
+  useEffect(() => {
+    if (!roomId) {
+      const roomIdUrl = getParams("roomId") as string;
+
+      if (roomIdUrl) {
+        navigate({
+          pathname: ROOT,
+          search: createSearchParams({ roomId: roomIdUrl }).toString(),
+        });
+
+        return;
+      }
+
+      navigate(ROOT);
+
+      return;
+    }
+
+    socketIo = io(config.REST_API_URL, {
+      query: { userName, roomId },
+    });
+
+    socketIo.on("connect", () => {
+      socketIo.on(ROOM_DATA, (data: Room) => {
+        updateRoom(data);
+      });
+
+      socketIo.on(ROOM_NEW_USER, (data: User) => {
+        console.log("data: ", data);
+        addUser(data);
+      });
+
+      socketIo.on(ROOM_USER_LOGOUT, (data: string) => {
+        console.log("data: ", data);
+        removeUser(data);
+      });
+
+      socketIo.on(EXCEPTION, (data: KnownError) => {
+        console.log("data: ", data.name);
+
+        if (
+          data.name === MISSING_ROOM.name ||
+          data.name === INVALID_ROOM.name
+        ) {
+          toast({
+            title: t("pages.voting_room.error_request_invalid_room_message"),
+            status: "error",
+            duration: null,
+          });
+
+          navigate(ROOT);
+        }
+      });
+    });
+  }, []);
 
   return (
     <Container>
