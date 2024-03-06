06d4418549ac7d0bc6077e722f3a2c2b152e155b
Welington Fidelis <welingtonfidelis@gmail.com>
2024-03-06 15:19:28 -0300

    Improve css on deck and card to mobile screen;

--- src/components/deck/styles.ts
+++ src/components/deck/styles.ts
@@ -3,9 +3,14 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   margin: 0.5rem;
-  overflow-x: overlay;
+  overflow-x: auto;
   max-width: calc(100vw - 1rem);
   align-items: flex-end;
   height: 10rem;
-  justify-content: center;
+  /* justify-content: center; */
+  justify-content: flex-start;
+
+  @media (max-width: 750px) {
+    height: 11rem;
+  }
 `;
